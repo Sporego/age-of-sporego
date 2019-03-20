c6977545bff6650464339cef6a1828e529a9c70f
Sporego <Hello@Filbert.me>
2019-03-19 20:41:02 -0400

    v1

---
+++ main.js
@@ -0,0 +1,28 @@
+function CharacterGen(name, profession, gender, age, strength, hp) {
+  this.name = name;
+  this.profession = profession;
+  this.gender = gender;
+  this.age = age;
+  this.strength = strength;
+  this.hp = hp;
+}
+
+function isAlive(character) {
+  if (character.hp > 0) {
+    console.log(character.hp);
+    return true;
+  }
+}
+
+function attack(character, target) {
+  target.hp = parseInt(target.hp) - parseInt(character.strength);
+}
+
+function levelUp(character) {
+  character.age = parseInt(character.age) + 1;
+  character.strength = parseInt(character.strength) + 5;
+  character.hp - parseInt(character.hp) + 25;
+}
+
+console.log("Welcome to Age of sporEGO");
+console.log("T");
